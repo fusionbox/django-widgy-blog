2514f4b8a886ab9c2abed21410b7d80d69fd3d4a
Scott Clark <sclark@fusionbox.com>
2014-09-17 11:03:52 -0600

    Docs changelog, update setup.py with more info

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 
 from widgy_blog.version import get_version
 
+__doc__ = "Reusable blog app for Django-Widgy"
+
 
 def get_absolutepath(fname):
     return os.path.join(os.path.dirname(__file__), fname)
@@ -16,12 +18,24 @@ def read(fname):
 setup(
     name='widgy-blog',
     version=get_version(),
+    author='Fusionbox, Inc.',
+    author_email='programmers@fusionbox.com',
     description=__doc__,
-    long_description=read('README.rst'),
+    long_description='\n\n'.join([read('README.rst'), read('CHANGES.rst')]),
+    license='BSD',
     packages=['widgy_blog'],
     include_package_data=True,
     install_requires=[
         'django-widgy',
     ],
     zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python 2.7',
+    ]
 )
