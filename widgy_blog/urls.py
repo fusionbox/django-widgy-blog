ec01e03f5f47b2617fd512016af6ee4d18b4a620
Gavin Wahl <gwahl@fusionbox.com>
2017-02-06 10:29:09 -0700

    django 1.10 support

--- widgy_blog/urls.py
+++ widgy_blog/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('widgy_blog.views',
-    url(r'^$', 'list', name='blog_list'),
-    url(r'^(?P<year>\d{4})/$', 'year_archive', name='blog_archive_year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'month_archive', name='blog_archive_month'),
-    url(r'^detail/(?P<slug>.+)/(?P<pk>\d+)/$', 'detail', name='blog_detail'),
-    url(r'^tag/(?P<tag>.+)/$', 'tag', name='blog_tag'),
-    url(r'^tag/?(P<tag>.+)/feed\.xml$', 'feed', name='blog_rss_feed'),
-    url(r'^feed\.xml$', 'feed', name='blog_rss_feed'),
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.list, name='blog_list'),
+    url(r'^(?P<year>\d{4})/$', views.year_archive, name='blog_archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', views.month_archive, name='blog_archive_month'),
+    url(r'^detail/(?P<slug>.+)/(?P<pk>\d+)/$', views.detail, name='blog_detail'),
+    url(r'^tag/(?P<tag>.+)/$', views.tag, name='blog_tag'),
+    url(r'^tag/?(P<tag>.+)/feed\.xml$', views.feed, name='blog_rss_feed'),
+    url(r'^feed\.xml$', views.feed, name='blog_rss_feed'),
     # widgy
-    url(r'^preview/(?P<pk>\d+)/(?P<root_node_pk>\d+)/$', 'detail', name='blog_detail_preview'),
-    url(r'^form/(?P<pk>\d+)/(?P<form_node_pk>\d+)/$', 'detail', name='blog_detail_form'),
-)
+    url(r'^preview/(?P<pk>\d+)/(?P<root_node_pk>\d+)/$', views.detail, name='blog_detail_preview'),
+    url(r'^form/(?P<pk>\d+)/(?P<form_node_pk>\d+)/$', views.detail, name='blog_detail_form'),
+]
